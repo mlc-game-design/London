ad0bd4593b069c90ea1928d62c45a2062ed0854a
Max Courneya <max@mlcgaming.com>
2018-12-29 14:30:18 -0600

    Added functionality to pull data from MapObject layer, interpret it, and create/place interactive objects into the map

---
+++ ProjectLondon/OverworldManager/MapTransitionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.Tiled;
+using Microsoft.Xna.Framework;
+
+namespace ProjectLondon
+{
+    public class MapTransitionHandler : MapObject
+    {
+        private string Type { get; set; }
+        public string DestinationMap { get; private set; }
+        public Vector2 DestinationPosition { get; private set; }
+        public Rectangle BoundingBox { get; private set; }
+
+        public MapTransitionHandler(string type, string destinationMap, Vector2 destinationPosition, Rectangle boundingBox)
+        {
+            Type = type;
+            DestinationMap = destinationMap;
+            DestinationPosition = destinationPosition;
+            BoundingBox = boundingBox;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Handle the Transition
+        }
+    }
+}
--- ProjectLondon/OverworldManager/OverworldManager.cs
+++ ProjectLondon/OverworldManager/OverworldManager.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework.Audio;
 using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Graphics;
+using System.Linq;
 
 namespace ProjectLondon
 {
@@ -15,42 +16,122 @@ namespace ProjectLondon
         ContentManager contentManager;
         GraphicsDevice graphicsDevice;
 
-        TiledMap mapCurrent;
-        TiledMapRenderer mapRenderer;
-        Camera2D mapCamera;
+        TiledMap MapCurrent;
+        TiledMapRenderer MapRenderer;
+        Camera2D MapCamera;
 
-        List<TiledMap> adjacentMaps;
+        List<TiledMap> AdjacentMaps;
+        List<MapObject> MapObjects;
+        
 
         public OverworldManager(GraphicsDevice gfxDevice, ContentManager content)
         {
             graphicsDevice = gfxDevice;
             contentManager = content;
 
-            mapCurrent = null;
-            mapRenderer = new TiledMapRenderer(graphicsDevice);
-            mapCamera = new Camera2D(graphicsDevice);
-            mapCamera.Origin = new Vector2(0, 0);
-            mapCamera.Position = new Vector2(0, 0);
-            mapCamera.Zoom = 4.0f;
+            MapCurrent = null;
+            MapRenderer = new TiledMapRenderer(graphicsDevice);
+            MapCamera = new Camera2D(graphicsDevice);
+            MapCamera.Origin = new Vector2(0, 0);
+            MapCamera.Position = new Vector2(0, 0);
+            MapCamera.Zoom = 4.0f;
 
-            adjacentMaps = new List<TiledMap>();
+            AdjacentMaps = new List<TiledMap>();
+            MapObjects = new List<MapObject>();
         }
 
+        /// <summary>
+        /// Loads TiledMap into memory by MapName string
+        /// </summary>
+        /// <param name="mapName"></param>
         public void LoadMap(string mapName)
         {
-            mapCurrent = contentManager.Load<TiledMap>(mapName);
-        } 
+            
+            MapCurrent = contentManager.Load<TiledMap>(mapName);
+        }
+        
+        /// <summary>
+        /// Iterates through list of adjacent maps, loading each one into memory
+        /// </summary>
+        /// <returns></returns>
+        private TiledMap LoadAdjacentMaps()
+        {
+            // Unload existing AdjacentMaps list
+            if(AdjacentMaps.Count > 0)
+            {
+                for(int i = AdjacentMaps.Count - 1; i >= 0; i--)
+                {
+                    AdjacentMaps.RemoveAt(i);
+                }
+            }
+
+            // Get the TiledMap Property for "adjacentMaps"
+            string mapListString = MapCurrent.Properties["adjacentMaps"];
+
+            // Separate the string at each semicolon (;) and create a List using set of strings
+            List<string> mapNames = mapListString.Split(';').ToList();
+            
+            // Take List<string> compiled and load each map without MapObjects 
+            foreach(string mapName in mapNames)
+            {
+                // Load Each Map
+
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Loads MapObjects for the Active Map
+        /// </summary>
+        private void LoadMapObjects()
+        {
+            TiledMapObjectLayer mapObjectLayer = (TiledMapObjectLayer)MapCurrent.GetLayer("MapObjects");
+
+            foreach (TiledMapObject mapObject in mapObjectLayer.Objects)
+            {
+                switch (mapObject.Name)
+                {
+                    case "mapPlayerSpawn":
+                        {
+                            // Run Player Spawn Code Here
+
+                            break;
+                        }
+                    case "mapTransition":
+                        {
+                            // Create Transition Objects
+                            float destinationX, destinationY;
+
+                            destinationX = Convert.ToInt32(mapObject.Properties["mapDestinationX"]);
+                            destinationY = Convert.ToInt32(mapObject.Properties["mapDestinationY"]);
+
+                            MapTransitionHandler mapTransition = new MapTransitionHandler(mapObject.Type, mapObject.Properties["mapDestination"],
+                                new Vector2((float)destinationX, (float)destinationY), new Rectangle((int)mapObject.Position.X, (int)mapObject.Position.Y, (int)mapObject.Size.Width, (int)mapObject.Size.Height));
+
+                            MapObjects.Add(mapTransition);
+                            break;
+                        }
+                    case "mapEntitySpawn":
+                        {
+                            // Spawn the Entity
+
+                            break;
+                        }
+                }
+            }
+        }
 
         public void Update(GameTime gameTime)
         {
-            mapRenderer.Update(mapCurrent, gameTime);
+            MapRenderer.Update(MapCurrent, gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin(transformMatrix: mapCamera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
+            spriteBatch.Begin(transformMatrix: MapCamera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
 
-            mapRenderer.Draw(mapCurrent, mapCamera.GetViewMatrix());
+            MapRenderer.Draw(MapCurrent, MapCamera.GetViewMatrix());
 
             spriteBatch.End();
         }
